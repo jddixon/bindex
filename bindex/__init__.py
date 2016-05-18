0a6a476b46685758ca22ac21769cf00427199d12
Jim Dixon <jddixon@gmail.com>
2016-05-18 11:24:29 -0700

    add bkp2U, .dvcz, etc

--- bindex/__init__.py
+++ bindex/__init__.py
@@ -9,13 +9,12 @@ from Crypto.Hash import SHA
 from Crypto.PublicKey import RSA
 
 from nlhtree import NLHLeaf
-from xlattice import u, SHA1_BIN_NONE, SHA2_BIN_NONE
+from xlattice import SHA1_BIN_NONE, SHA2_BIN_NONE
 from xlattice.crypto import (
     AES_BLOCK_SIZE, addPKCS7Padding, stripPKCS7Padding)
 
 __all__ = ['__version__', '__version_date__',
            ]
 
-__version__      = '0.0.10'
-__version_date__ = '2016-04-22'
-
+__version__ = '0.0.11'
+__version_date__ = '2016-05-18'
