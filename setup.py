a9f337048b2c9c1f0666829f40f60845a155079a
Jim Dixon <jddixon@gmail.com>
2017-07-09 16:04:38 -0700

    edit config files

--- setup.py
+++ setup.py
@@ -3,18 +3,36 @@
 """ Do setup for bindex package. """
 
 import re
-from distutils.core import setup
+
+# BEGIN NEW
+from glob import glob
+from os.path import basename, dirname, join, splitext
+from setuptools import find_packages, setup
+# END NEW
+
 __version__ = re.search(r"__version__\s*=\s*'(.*)'",
                         open('src/bindex/__init__.py').read()).group(1)
 
-# see http://docs.python.org/distutils/setupscript.html
+# see
+# setuptools.readthedocs.io/en/latest/setuptools.html#new-and-changed-setup-keywords
+
+with open('README.md', 'r') as file:
+    long_desc = file.read()
 
 setup(name='bindex',
       version=__version__,
       author='Jim Dixon',
       author_email='jddixon@gmail.com',
-      py_modules=[],
-      packages=['src/bindex'],
+
+      # BEGIN NEW
+      long_description=long_desc,
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+      include_package_data=False,
+      zip_safe=False,
+      # END NEW
+
       # following could be in scripts/ subdir
       scripts=[],
       description="index content-keyed files",
---
+++ src/bindex/__init__.py
@@ -0,0 +1,24 @@
+# bindex/__in(it__.py
+
+"""
+Build index by path into a content-keyed data store.
+"""
+
+import binascii
+import hashlib
+
+from pbkdf2 import PBKDF2       # note name of package is u/c
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+
+from nlhtree import NLHLeaf
+from xlattice import SHA1_BIN_NONE, SHA2_BIN_NONE
+from xlattice.crypto import (
+    AES_BLOCK_SIZE, addPKCS7Padding, stripPKCS7Padding)
+
+__all__ = ['__version__', '__version_date__',
+           ]
+
+__version__ = '0.0.19'
+__version_date__ = '2017-07-09'
