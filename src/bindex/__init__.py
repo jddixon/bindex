ba40858c4dac491410dcded5fcbdad6d11244544
Jim Dixon <jddixon@gmail.com>
2018-02-27 10:32:00 -0800

    fix setup.py, tox config files

--- src/bindex/__init__.py
+++ src/bindex/__init__.py
@@ -5,20 +5,14 @@ Build index by path into a content-keyed data store.
 """
 
 # import binascii
-# import hashlib
 
-# from pbkdf2 import PBKDF2       # note name of package is u/c
-# from Crypto.Cipher import AES
-# from Crypto.Hash import SHA
-# from Crypto.PublicKey import RSA
-#
 # from nlhtree import NLHLeaf
 # from xlattice import SHA1_BIN_NONE, SHA2_BIN_NONE
-# from xlattice.crypto import (
-#     AES_BLOCK_SIZE, addPKCS7Padding, stripPKCS7Padding)
+# from xlcrypto import (
+#     AES_BLOCK_BYTES, addPKCS7Padding, stripPKCS7Padding)
 
 __all__ = ['__version__', '__version_date__',
            ]
 
-__version__ = '0.0.23'
-__version_date__ = '2017-12-05'
+__version__ = '0.0.24'
+__version_date__ = '2018-02-27'
